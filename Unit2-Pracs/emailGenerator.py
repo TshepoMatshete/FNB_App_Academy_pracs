7927e58249efdf84359908a5a9ce462269361b3a
Rodgers M <rodgersmatshete0@gmail.com>
2026-07-17 23:12:10 +0200

    replaced the split method with the stripe method

--- Unit2-Pracs/emailGenerator.py
+++ Unit2-Pracs/emailGenerator.py
@@ -1,10 +1,10 @@
 # This is an email generator for universities in south africa. It takes the first and last name, and the name of the university of the user and generates an email address in the format: first.last@university.ac.za
 
 
-firstName = input("Please enter your first name: ").split()
-lastName = input("Please enter your last name: ").split()
-university = input("Please enter the name of your university: ").split()
+firstName = input("Please enter your first name: ").strip()
+lastName = input("Please enter your last name: ").strip()
+university = input("Please enter the name of your university: ").strip()
 
-email = f"{firstName[0]}.{lastName[0]}1@{university[0]}.ac.za"
+email = f"{firstName}.{lastName}1@{university}.ac.za"
 
 print(f"Your generated email address is: {email.lower()}")
